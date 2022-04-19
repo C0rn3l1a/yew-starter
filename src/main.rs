54b565aa814a4b145ded09be9c0e4f4b3502c954
C0rn3l1a <nachoborello97@gmail.com>
2022-04-19 09:35:41 -0300

    fetch videos from yew tutorial page

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,34 @@ mod components;
 use crate::components::common::nav::NavBar;
 use crate::components::video_details::VideoDetails;
 use crate::components::video_list::{Video, VideosList};
+use reqwasm::http::Request;
 use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
-    let videos = vec![
-        Video {
-            id: 1,
-            title: "Building and breaking things".to_string(),
-            speaker: "John Doe".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 2,
-            title: "The development process".to_string(),
-            speaker: "Jane Smith".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 3,
-            title: "The Web 7.0".to_string(),
-            speaker: "Matt Miller".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 4,
-            title: "Mouseless development".to_string(),
-            speaker: "Tom Jerry".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-    ];
+    let videos = use_state(|| vec![]);
+    {
+        let videos = videos.clone();
+        use_effect_with_deps(
+            move |_| {
+                let videos = videos.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_videos: Vec<Video> =
+                        // Request::get("https://yew.rs/tutorial/data.json")
+                        Request::get("/tutorial/data.json")
+                            .send()
+                            .await
+                            .unwrap()
+                            .json()
+                            .await
+                            .unwrap();
+                    videos.set(fetched_videos);
+                });
+                || ()
+            },
+            (),
+        );
+    }
 
     let selected_video = use_state(|| None);
 
@@ -55,7 +53,7 @@ fn app() -> Html {
                     <h1>{ "List of Videos" }</h1>
                 </header>
                 <section>
-                <VideosList videos={videos} on_click={on_video_select.clone()} />
+                <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
                 </section>
                 <section>
                     { for details }
